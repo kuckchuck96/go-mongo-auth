2de9a3d6d0dc5b4c624fda83c8f13ee8b9e3ea9e
kuckchuck96 <50999920+kuckchuck96@users.noreply.github.com>
2022-11-05 19:25:21 +0530

    Created common context

--- internal/database/mongo_database.go
+++ internal/database/mongo_database.go
@@ -48,25 +48,29 @@ func NewMongoClient(mongoConfig config.Mongo) (IMongoClient, error) {
 	}, nil
 }
 
-func (c *MongoClient) GetCollection(collectionName string) *mongo.Collection {
-	collection := c.client.Database(c.mongoConfig.Database).Collection(collectionName)
+func getContextWithTimeout(client *MongoClient) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), client.mongoConfig.ContextTimeout)
+}
+
+func (client *MongoClient) GetCollection(collectionName string) *mongo.Collection {
+	collection := client.client.Database(client.mongoConfig.Database).Collection(collectionName)
 	return collection
 }
 
-func (c *MongoClient) CreateOneDocument(collectionName string, doc any) (*mongo.InsertOneResult, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), c.mongoConfig.ContextTimeout)
+func (client *MongoClient) CreateOneDocument(collectionName string, doc any) (*mongo.InsertOneResult, error) {
+	ctx, cancel := getContextWithTimeout(client)
 	defer cancel()
 
-	collection := c.GetCollection(collectionName)
+	collection := client.GetCollection(collectionName)
 
 	return collection.InsertOne(ctx, doc)
 }
 
-func (c *MongoClient) FindOneDocument(collectionName string, filter primitive.M) *mongo.SingleResult {
-	ctx, cancel := context.WithTimeout(context.Background(), c.mongoConfig.ContextTimeout)
+func (client *MongoClient) FindOneDocument(collectionName string, filter primitive.M) *mongo.SingleResult {
+	ctx, cancel := getContextWithTimeout(client)
 	defer cancel()
 
-	collection := c.GetCollection(collectionName)
+	collection := client.GetCollection(collectionName)
 
 	return collection.FindOne(ctx, filter)
 }
